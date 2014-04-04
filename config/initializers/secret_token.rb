bb3f2f7051f6fd2c632c0a0571a226c70099c803
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-04-04 08:52:22 +0200

    drafts the app with mongodb and haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreamingPastie::Application.config.secret_key_base = '8b1fa7947a68f14cf8cb688299433a9db11fd65d6ff495c7f468016efe56e49987d38393179a0d57e27e9df5efd672a6b6a25f3b3dfbaebf7743b951ae8a11ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StreamingPastie::Application.config.session_store :cookie_store, key: '_streaming-pastie_session'
